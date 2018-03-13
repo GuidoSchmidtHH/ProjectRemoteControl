b1ca894bbab947e2101aeb7879e3c79fb393cef2
GuidoSchmidtHH <36132036+GuidoSchmidtHH@users.noreply.github.com>
2018-03-13 17:38:52 +0000

    Add files via upload

--- RemCtrlDB.java
+++ RemCtrlDB.java
@@ -9,14 +9,16 @@ class RemCtrlDB
 		try
 		{  
 
-			Class.forName("com.mysql.jdbc.Driver");  
+			//Class.forName("com.mysql.jdbc.Driver");  
+			
+			Class.forName("mysql-connector-java-5.1.38-bin");
 			
 			Connection con=DriverManager.getConnection(  
-					"jdbc:mysql://localhost:3306/Test","root","Ph10t10n");  
+					"jdbc:mysql://localhost:3306/Test","root","********");  
 			
 			//here sonoo is database name, root is username and password  
 			Statement stmt=con.createStatement();  
-			ResultSet rs=stmt.executeQuery("select * from emp");  
+			ResultSet rs=stmt.executeQuery("select * from TestRemote");  
 			while(rs.next())  
 				System.out.println(rs.getInt(1)+"  "+rs.getString(2)+"  "+rs.getString(3));  
 			con.close();  
